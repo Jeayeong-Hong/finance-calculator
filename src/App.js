c0e18b1ab99803e146aa6de13c65d4d825e491f7
Jeayeong_Hong <hong612644@gmail.com>
2025-03-21 15:55:32 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [amount, setAmount] = useState('');
+  const [rate, setRate] = useState('');
+  const [years, setYears] = useState('');
+  const [result, setResult] = useState('');
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+
+    const reponse = await fetch('http://127.0.0.1:5000/api/calculate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount, rate, years })
+    });
+
+    const data = await reponse.json();
+    setResult(data.result);
+  };
+  return(
+    <div>
+      <h1>Finance-calculator</h1>
+      <from onSubmit={handlesubmit}>
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder="Amount"
+        />
+        <input
+          type="number"
+          value={years}
+          onChange={(e) => setRate(e.target.value)}
+          placeholder="Rate"
+        />
+        <button type="submit">Calculate</button>
+      </from>
+      {result && <p>reslut: {result}</p>}
     </div>
-  );
+  )
 }
 
 export default App;
